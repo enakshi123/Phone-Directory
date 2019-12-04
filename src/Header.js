c23502335fd699b661e1b3933e013ac3524369e7
enakshi <44223606+enakshi123@users.noreply.github.com>
2019-12-04 15:11:08 +0530

    Styling: Styled 'Header' (External)

--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,11 @@ import React, {Component} from 'react';
  class Header extends Component{
     render(){
     // const Header = function () {
-       const headerStyle = {
-            textAlign: 'center',
-            padding: 20,
-            background: '#000',
-            color: '#fff',
-            textTransform: 'uppercase'
-        };
+       
         return(
-            <div style = {headerStyle}>
-                {/* /* {this.props.title}  */ }
+            // <div style = {headerStyle}>
+                /* /* {this.props.title}  */ 
+                <div className="header">
                 Phone Directory
             </div>
         )
