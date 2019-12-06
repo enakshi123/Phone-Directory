832895445ce262c14ece14044820f796edaeef7f
enakshi <44223606+enakshi123@users.noreply.github.com>
2019-12-06 11:45:50 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,7 @@ import './Header.css';
     // const Header = function () {
         const Header = function (props) {
         return(
-            // <div style = {headerStyle}>
-                /* /* {this.props.title}  */ 
+           
                 <div className="header">
                  {props.heading}
             </div>
